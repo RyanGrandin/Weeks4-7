d306936938a2a0c9a239cf53c8d9018be2c69ec1
Ryan Grandin <rmgrandin8@gmail.com>
2026-02-11 11:25:06 -0500

    Filled in carMovement script: moves along the x axis. when it exceeds the edge of the screeen, returns to starting postiton. Filled in frogMovement script: moves up, down, left, and right by one unit according to arrow keys. If the frog is hit by a car, it is set inactive and text from a UI canvas is set active.

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
+    public float speed = 3f;
+    private float startPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // moves the object
+        Vector2 newPos = transform.position;
+        newPos.x += speed * Time.deltaTime;
+        transform.position = newPos;
+
+        // sets the object back to its starting position if it exits the screen
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(newPos);
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            newPos.x = startPos;
+        }
+
+        transform.position = newPos;
     }
 }
--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -1,7 +1,18 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FrogMovement : MonoBehaviour
 {
+    public GameObject car1;
+    public GameObject car2;
+    public GameObject car3;
+    float car1Distance;
+    float car2Distance;
+    float car3Distance;
+    public TextMeshProUGUI endText;
+    public GameObject endTextObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,35 @@ public class FrogMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newPos = transform.position;
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            newPos.y += 1;
+        }
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            newPos.y -= 1;
+        }
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            newPos.x -= 1;
+        }
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            newPos.x += 1;
+        }
+        transform.position = newPos;
+
+        // gets distance of the frog from the cars
+        car1Distance = Vector2.Distance(transform.position, car1.transform.position);
+        car2Distance = Vector2.Distance(transform.position, car2.transform.position);
+        car3Distance = Vector2.Distance(transform.position, car3.transform.position);
+
+        if (car1Distance < 1 || car2Distance < 1 || car3Distance < 1)
+        {
+            endText.text = "You Lose";
+            endTextObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
     }
 }
