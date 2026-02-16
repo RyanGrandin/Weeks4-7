319c5f4de75d72ef69b1ea6775148bf0af51211f
Ryan Grandin <rmgrandin8@gmail.com>
2026-02-16 17:21:18 -0500

    Made the apporpriate water turn on while any key is pressed.

--- Assets/Scripts/HoseScript.cs
+++ Assets/Scripts/HoseScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class HoseScript : MonoBehaviour
 {
     bool switchSide = false;
+    public GameObject water;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,22 +18,35 @@ public class HoseScript : MonoBehaviour
         // put position into a new Vector2 so that its values may be changed
         Vector2 newPos = transform.position;
 
+        // get mouse position
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
         // code applies if the left hose is switched to
         if (switchSide == false && newPos.x < 0)
         {
-            // get mouse position
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
             newPos.y = mousePos.y;
+            if (Keyboard.current.anyKey.isPressed)
+            {
+                water.SetActive(true);
+            }
+            else
+            {
+                water.SetActive(false);
+            }
         }
 
         // code applies if the right hose is switched to
         if (switchSide == true && newPos.x > 0)
         {
-            // get mouse position
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
             newPos.y = mousePos.y;
+            if (Keyboard.current.anyKey.isPressed)
+            {
+                water.SetActive(true);
+            }
+            else
+            {
+                water.SetActive(false);
+            }
         }
 
         transform.position = newPos;
