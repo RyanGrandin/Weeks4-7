9bad6e5a86e81cb91f53e2ceec0d39affcb2267a
Ryan Grandin <rmgrandin8@gmail.com>
2026-02-16 19:13:52 -0500

    When the water rolls over the fire, another fire should be instantiated and the current fire should be destroyed. However, the fire doesn't currently function because there is no way for the fireScript to get the references it needs. This might be accomplished using a fire spawner.

--- Assets/Scripts/FireScript.cs
+++ Assets/Scripts/FireScript.cs
@@ -4,10 +4,14 @@ public class FireScript : MonoBehaviour
 {
     int whichSide;
     public GameObject buildingBody;
+    public SpriteRenderer water;
+    public GameObject firePrefab;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        buildingBody.GetComponent<GameObject>();
+        
         Vector2 newPos = transform.position;
 
         // sets the object's x position to a random side of the building (left or right)
@@ -30,6 +34,10 @@ public class FireScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // runs if there is fire in the water
+        if (water.bounds.Contains(transform.position)) {
+            Instantiate(firePrefab);
+            Destroy(gameObject);
+        }
     }
 }
