58cb52231c7f5330a56af8ee4748bc2aa569abe3
Ryan Grandin <rmgrandin8@gmail.com>
2026-02-17 21:08:53 -0500

    Moved fire position code into a Vector2, return function in the HoseScript. Put the code for when the water contains fire into the Update() of the HoseScript. Within the Start() of HoseScript calls the FirePosition() method for the first fire on screen. deleted the, now empty, FireScript and Fire Spawner GameObject. There are now two fires on screen at once and they are destroyed when either water, regardless of which side, is at about the same y position as the fire even if the water is inactive.

--- Assets/Scripts/HoseScript.cs
+++ Assets/Scripts/HoseScript.cs
@@ -5,11 +5,18 @@ public class HoseScript : MonoBehaviour
 {
     bool switchSide = false;
     public GameObject water;
-    
+    public SpriteRenderer waterSR;
+    int whichSide;
+    public GameObject buildingBody;
+    public GameObject firePrefab;
+    public GameObject fire;
+    public GameObject firstFire;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        fire = firstFire;
+        fire.transform.position = FirePosition();
     }
 
     // Update is called once per frame
@@ -54,11 +61,40 @@ public class HoseScript : MonoBehaviour
         }
 
         transform.position = newPos;
+
+        // runs if there is fire in the water
+        if (waterSR.bounds.Contains(FirePosition()))
+        {
+            Debug.Log("Water contains fire. Fire Position = " + FirePosition());
+            Destroy(fire);
+            fire = Instantiate(firePrefab, FirePosition(), Quaternion.identity);
+        }
     }
 
     // change the hose that the player controls
     public void SwitchSide()
     {
         switchSide = !switchSide;
+    }
+
+    Vector2 FirePosition()
+    {
+        Vector2 firePos = new Vector2();
+
+        // sets the fire's x position to a random side of the building (left or right)
+        whichSide = Random.Range(0, 2);
+        if (whichSide == 0)
+        {
+            firePos.x = buildingBody.transform.position.x - buildingBody.transform.localScale.x / 2;
+        }
+        if (whichSide == 1)
+        {
+            firePos.x = buildingBody.transform.position.x + buildingBody.transform.localScale.x / 2;
+        }
+
+        // sets the fire's y position to something random within the height of the building
+        firePos.y = Random.Range(buildingBody.transform.position.y - buildingBody.transform.localScale.y / 2, buildingBody.transform.position.y + buildingBody.transform.localScale.y / 2);
+
+        return firePos;
     }
 }
